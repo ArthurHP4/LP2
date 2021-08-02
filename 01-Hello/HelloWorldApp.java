d9141e0ecfca8bfd2ee563190fed80868af41e80
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-08-02 10:10:39 -0300

    Update HelloWorldApp.java

--- 01-Hello/HelloWorldApp.java
+++ 01-Hello/HelloWorldApp.java
@@ -1,5 +1,5 @@
-class HelloWorldApp{
-	public static void main(String[] args){
-		System.out.println("HelloWorld");
-	}
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+     }
 }
