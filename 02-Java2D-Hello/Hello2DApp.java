4366d5da7097a3c928bcef2c7d5f0b5cb7c2bcb9
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-08-09 10:22:21 -0300

    Update Hello2DApp.java

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -20,15 +20,25 @@ class Hello2DFrame extends JFrame {
         this.setTitle("Java2D - Hello World!");
         this.setSize(350, 350);
         this.setVisible(true);
+		this.getContentPane().setBackground(Color.BLACK);
+		
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
+        g2d.setPaint(Color.yellow);
         int w = getWidth();
         int h = getHeight();
         g2d.drawLine(0,0, w,h);
         g2d.drawLine(0,h, w,0);
+		
+		g2d.drawLine(10,20,40,80);
+		g2d.setPaint(Color.red);
+		g2d.drawRect( getWidth()/2 , getHeight()/2, 50, 100);
+		
+		g2d.setPaint(Color.green);
+		//g2d.drawOval(getWidth()/2,getHeight()/2,100,100);
+		g2d.fillOval(getWidth()/2,getHeight()/2,100,100);
     }
 }
