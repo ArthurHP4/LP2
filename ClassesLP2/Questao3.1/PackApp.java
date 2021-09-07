475a342be8757ad5c0911c8a36f34df8a8485f78
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-07 15:32:25 -0300

    Update PackApp.java

--- ClassesLP2/Questao3.1/PackApp.java
+++ ClassesLP2/Questao3.1/PackApp.java
@@ -2,37 +2,40 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-import figuras.*;
+import figures.*;
 
-class PackApp {
-    public static void main (String[] args) {
-        PackFrame frame = new PackFrame();
-        frame.setVisible(true);
-    }
+class PackApp{
+	public static void main (String[] args){
+		PackFrame frame = new PackFrame();
+		frame.setVisible(true);
+	}
 }
 
-class PackFrame extends JFrame {
-    Rect r1;
-    Ellipse e1;
-    Triangulo t1;
-    PackFrame () {
-        this.addWindowListener (
-            new WindowAdapter() {
-                public void windowClosing (WindowEvent e) {
-                    System.exit(0);
-                }
-            }
-        );
-        this.setTitle("Java Packages");
-        this.setSize(350, 350);
-        this.r1 = new Rect(50,50, 100,30);
-        this.e1 = new Ellipse(50,100, 100,30);
-    }
+class PackFrame extends JFrame{
+	Rect r1;
+	Ellipse e1;
+	Triangulo t1;
+	public PackFrame (){
+		this.addWindowListener(
+			new WindowAdapter(){
+				public void windowClosing (WindowEvent e){
+					System.exit(0);
+				}
+			}
+		);
+		
+		this.setTitle("Java Packages");
+		this.setSize(350,350);
+		this.r1 = new Rect(50,50,100,30);
+		this.e1 = new Ellipse(50,100,100,30);
+		this.t1 = new Triangulo(20,40,20,80,40,80);
+	}
+	
+	public void paint (Graphics g){
+		super.paint(g);
+		this.r1.paint(g);
+		this.e1.paint(g);
+		this.t1.paint(g);
 
-    public void paint (Graphics g) {
-        super.paint(g);
-        this.r1.paint(g);
-        this.e1.paint(g);
-        this.t1.paint(g);
-    }
+	}
 }
