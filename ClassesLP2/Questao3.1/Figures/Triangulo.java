d2b57968ffdb39e5d6f911d9eed525137598cd03
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-07 15:33:59 -0300

    Create Triangulo.java

---
+++ ClassesLP2/Questao3.1/Figures/Triangulo.java
@@ -0,0 +1,28 @@
+package figures;
+
+import java.awt.*;
+
+public class Triangulo {
+	int x1, y1; //Primeira vertice do triângulo
+	int x2, y2; //Segunda vertice do triângulo
+	int x3, y3; //Terceira vertice do triângulo
+
+	public Triangulo (int x1, int y1, int x2, int y2, int x3, int y3){
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.x3 = x3;
+		this.y3 = y3;
+
+	}
+	
+	public void paint(Graphics g){
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawLine(this.x1,this.y1,this.x2,this.y2);
+		g2d.drawLine(this.x2,this.y2,this.x3,this.y3);
+		g2d.drawLine(this.x3,this.y3,this.x1,this.y1);
+
+	}
+}
+
