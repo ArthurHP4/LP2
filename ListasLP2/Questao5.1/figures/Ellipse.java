1225b609d9645afcb23cebb5ea882bba4730ea65
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-11 18:40:37 -0300

    Create Ellipse.java

---
+++ ListasLP2/Questao5.1/figures/Ellipse.java
@@ -0,0 +1,32 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+
+public class Ellipse {
+	int x, y;
+	int w, h;
+	int rC, gC, bC;
+	int rF, gF, bF;
+	public Ellipse (int x, int y, int w, int h, int rC, int gC, int bC, int rF, int gF, int bF){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.rC = rC; this.gC = gC; this.bC = bC; // cores RGB do conrtorno
+		this.rF = rF; this.gF = gF; this.bF = bF; //cores RGB do fundo
+	}
+	
+
+	public void paint (Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(new Color(this.rC, this.gC, this.bC));
+		g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+		g2d.setColor(new Color(this.rF, this.gF, this.bF));
+		g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+	}
+
+}
+
+
