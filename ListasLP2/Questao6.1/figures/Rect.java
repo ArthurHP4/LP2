ac5606a83ef2aaedc561df59b9a7655fdc013f03
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-19 23:51:08 -0300

    Add files via upload

---
+++ ListasLP2/Questao6.1/figures/Rect.java
@@ -0,0 +1,25 @@
+package figures;
+
+import java.awt.*;
+
+
+public class Rect extends Figure{
+	
+	public Rect (int x, int y, int w, int h, int rC, int gC, int bC, int rF, int gF, int bF){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.rC = rC; this.gC = gC; this.bC = bC; // cores RGB do conrtorno
+		this.rF = rF; this.gF = gF; this.bF = bF; //cores RGB do fundo
+	}
+	
+	public void paint (Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(new Color(this.rC, this.gC, this.bC));
+		g2d.fillRect(this.x,this.y,this.w,this.h);
+		g2d.setColor(new Color(this.rF, this.gF, this.bF));
+		g2d.drawRect(this.x,this.y,this.w,this.h);
+
+	}
+}
---
+++ ListasLP2/Questao6.1/figures/Triangulo.java
@@ -0,0 +1,33 @@
+package figures;
+
+import java.awt.*;
+
+public class Triangulo extends Figure {
+	
+	int x1, y1; //Primeira vertice do triângulo
+	int x2, y2; //Segunda vertice do triângulo
+	int x3, y3; //Terceira vertice do triângulo
+	
+	
+
+	public Triangulo (int x1, int y1, int x2, int y2, int x3, int y3,int rC, int gC, int bC){
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.x3 = x3;
+		this.y3 = y3;
+		this.rC = rC; this.gC = gC; this.bC = bC; // cores RGB do conrtorno
+
+	}
+	
+	public void paint(Graphics g){
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(new Color(this.rC, this.gC, this.bC));
+		g2d.drawLine(this.x1,this.y1,this.x2,this.y2);
+		g2d.drawLine(this.x2,this.y2,this.x3,this.y3);
+		g2d.drawLine(this.x3,this.y3,this.x1,this.y1);
+
+	}
+}
+
