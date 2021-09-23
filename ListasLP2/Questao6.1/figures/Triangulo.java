74c85df388aff1ebb587cbfb9af299822f1553d8
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-23 19:40:32 -0300

    Update Triangulo.java

--- ListasLP2/Questao6.1/figures/Triangulo.java
+++ ListasLP2/Questao6.1/figures/Triangulo.java
@@ -11,13 +11,13 @@ public class Triangulo extends Figure {
 	
 
 	public Triangulo (int x1, int y1, int x2, int y2, int x3, int y3,int rC, int gC, int bC){
+		super(rC,gC,bC);
 		this.x1 = x1;
 		this.y1 = y1;
 		this.x2 = x2;
 		this.y2 = y2;
 		this.x3 = x3;
 		this.y3 = y3;
-		this.rC = rC; this.gC = gC; this.bC = bC; // cores RGB do conrtorno
 
 	}
 	
