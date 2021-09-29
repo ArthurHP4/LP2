5d0a9337100310f87a4c89adf1e8a1862f101ea2
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-29 04:38:41 -0300

    Update Triangulo.java

--- ProjetoAppPart1/figures/Triangulo.java
+++ ProjetoAppPart1/figures/Triangulo.java
@@ -4,23 +4,15 @@ import java.awt.*;
 
 
 public class Triangulo extends Figure{
-	int x2,y2;
-	int x3,y3;
-	Color cor2;
-	public Triangulo(int x, int y, int x2, int y2, int x3, int y3, Color cor1,Color cor2) {
-		super(x, y, cor1);
-		this.cor2 = cor2;
-		this.x2 = x2;
-		this.y2 = y2;
-		this.x3 = x3;
-		this.y3 = y3;
+	public Triangulo(int x, int y, int w, int h,Color cor1,Color cor2) {
+		super(x,y,w,h,cor1,cor2);
 	}
 
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(cor1);
-		int xPoints[] = {x, x2, x3};
-		int yPoints[] = {y, y2, y3};
+		int xPoints[] = {this.x, ((this.w)/2) + (this.x), (this.w) + (this.x)};
+        int yPoints[] = {this.y + h, (this.y + h) - (this.h), this.y + h};
 		g2d.fillPolygon(xPoints, yPoints, 3);
 		g2d.setColor(cor2);
 		
@@ -29,8 +21,8 @@ public class Triangulo extends Figure{
 
 	
 	public void contains(int x, int y) {
-		int xPoints[] = {x, x2, x3};
-		int yPoints[] = {y, y2, y3};
+		int xPoints[] = {this.x, ((this.w)/2) + (this.x), (this.w) + (this.x)};
+        int yPoints[] = {this.y + h, (this.y + h) - (this.h), this.y + h};
 		Polygon p = new Polygon(xPoints, yPoints, 3);
 	}
 	
