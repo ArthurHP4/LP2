4bddcb124ba571d912c229866c6b15f90a37429c
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-29 19:30:37 -0300

    Update ProjetoApp.java

--- ProjetoAppPart1/ProjetoApp.java
+++ ProjetoAppPart1/ProjetoApp.java
@@ -42,50 +42,6 @@ class ProjetoFrame extends JFrame{
 					
 					//INICIO CORES
 					if(focus != null){
-						if(evt.getKeyChar() == '6'){import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Random;
-import java.awt.geom.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import figures.*;
-
-class ProjetoApp{
-	public static void main (String[] args){
-		ProjetoFrame frame = new ProjetoFrame();
-		frame.setVisible(true);
-	}
-	
-}
-
-class ProjetoFrame extends JFrame{
-	ArrayList<Figure> figs = new ArrayList<Figure>();
-    Random rand = new Random();
-	Figure focus = null;
-	Point pontaMouse = null;
-
-	public ProjetoFrame (){
-		this.addWindowListener(
-			new WindowAdapter(){
-				public void windowClosing (WindowEvent e){
-					System.exit(0);
-				}
-			}
-		);
-		
-		this.addKeyListener(
-			new KeyAdapter(){
-				public void keyPressed (KeyEvent evt) {
-					
-					//INICIO CORES
-					if(focus != null){
 						if(evt.getKeyChar() == '6'){
 							focus.cor1 =  Color.decode("#800080");
 						}
@@ -117,16 +73,24 @@ class ProjetoFrame extends JFrame{
 					
 					//Inicio Eventos teclado
 					if(evt.getKeyChar() == 'r') {	
-                        figs.add(new Rect(x,y,w,h,Color.white,Color.black));
+                        figs.add(new Rect(x,y,w,h,Color.black,Color.white));
+						focus=figs.get(figs.size() - 1);
+
 					}
 					else if(evt.getKeyChar() == 'e') {	
-                        figs.add(new Ellipse(x,y,w,h,Color.white,Color.black));
+                        figs.add(new Ellipse(x,y,w,h,Color.black,Color.white));
+						focus=figs.get(figs.size() - 1);
+
 					}
-					else if(evt.getKeyChar() == 'l') {		
-						figs.add(new Line(x,y,x+100,y+100,Color.white,Color.white));
+					else if(evt.getKeyChar() == 'p') {		
+						figs.add(new Pentagono(x,y,w,h,Color.black,Color.white));
+						focus=figs.get(figs.size() - 1);
+
 					}					
 					else if(evt.getKeyChar() == 't') {	
-						figs.add(new Triangulo(x,y,w,h,Color.white,Color.black));
+						figs.add(new Triangulo(x,y,w,h,Color.black,Color.white));
+						focus=figs.get(figs.size() - 1);
+
 					}	
 					//Inicia propriedade de aumento ou diminuição da figura foco
 					if(evt.getKeyChar() == '+') {		
@@ -172,7 +136,6 @@ class ProjetoFrame extends JFrame{
                         for( Figure fig: figs){
                             if ((focus == null) || (focus!=null)){
                                 focus=fig;
-								focus.cor2 = Color.green;			
 								figs.add(focus);
 								figs.remove(focus);
 								repaint();
@@ -190,164 +153,34 @@ class ProjetoFrame extends JFrame{
 		this.addMouseListener (
             new MouseAdapter() {
                 public void mousePressed (MouseEvent evt) {
-                    pontaMouse = getMousePosition();
-					Color foc = Color.decode("#800000");
-                    for (Figure fig: figs) {		
-						if (fig.clicked(pontaMouse.x,pontaMouse.y)) {
-							focus = fig;
-							focus.cor2 = Color.green;
-						}
-						if(focus != null) {
-							figs.add(focus);
-							figs.remove(focus);	
-						}
-					}
-				} 
-            }
-			);	
-			this.addMouseMotionListener(
-			new MouseMotionAdapter(){
-				public void mouseDragged (MouseEvent evt) {
-					pontaMouse = getMousePosition();
-					if (focus != null) {  
-						
-                        figs.remove(focus);
+                focus = null;
+				int x = evt.getX();
+				int y = evt.getY();
+					
+				for (Figure fig: figs) {
+					if (fig.clicked(x,y)) { 				
+						focus = fig; 
+						repaint();
 						figs.add(focus);
-						focus.x = pontaMouse.x - focus.w;
-						focus.y = pontaMouse.y - focus.h;		
-						repaint();				
-					}
-				}				
-			}
-			);
-		this.setTitle("Projeto App");
-		this.setSize(550,550);	
-		this.getContentPane().setBackground(Color.BLACK);		
-	}
-	
-	public void paint (Graphics g){
-		super.paint(g);
-        for (Figure fig: this.figs) {
-            fig.paint(g);
-        }
-	}
-}
-
-							focus.cor1 =  Color.decode("#800080");
-						}
-						else if(evt.getKeyChar() == '5') {	
-							focus.cor1 = Color.decode("#800000");
-						}
-						else if(evt.getKeyChar() == '4') {	
-							focus.cor1 = Color.decode("#ffff00");
-
-						}
-						else if(evt.getKeyChar() == '3') {	
-							focus.cor2 = Color.decode("#800080");
-						}
-						else if(evt.getKeyChar() == '2') {	
-							focus.cor2 = Color.decode("#800000");
-						}
-						else if(evt.getKeyChar() == '1') {	
-							focus.cor2 = Color.decode("#ffff00");
-						}	
-					}
-					//FIM CORES*/
-					
-					//Determinar a criação das figuras tomando a ponta do mouse como referencia
-					pontaMouse = getMousePosition();
-					int x = pontaMouse.x;
-					int y = pontaMouse.y;
-					int w = 80;
-					int h = 50;
-					
-					//Inicio Eventos teclado
-					if(evt.getKeyChar() == 'r') {	
-                        figs.add(new Rect(x,y,w,h,Color.white,Color.black));
-					}
-					else if(evt.getKeyChar() == 'e') {	
-                        figs.add(new Ellipse(x,y,w,h,Color.white,Color.black));
-					}
-					else if(evt.getKeyChar() == 'l') {		
-						figs.add(new Line(x,y,x+100,y+100,Color.white,Color.white));
-					}					
-					else if(evt.getKeyChar() == 't') {	
-						figs.add(new Triangulo(x,y,w,h,Color.white,Color.black));
-					}	
-					//Inicia propriedade de aumento ou diminuição da figura foco
-					if(evt.getKeyChar() == '+') {		
-						focus.w = focus.w + 10;
-						focus.h = focus.h + 10;
-					}					
-					else if(evt.getKeyChar() == '-') {		
-						focus.w = focus.w - 10;
-						focus.h = focus.h - 10;
-					}					
-					//Deleta a figura
-					if(figs.size() != 0){
-						if(evt.getKeyChar() == 'd') {	
-							if(figs.size() != 0){						
-								figs.remove(figs.size() - 1);
-								repaint();
-							}
-						}
-					}
-					//Limpar o focus
-					if(evt.getKeyChar() == 'c') {	
-                        figs.add(focus);
 						figs.remove(focus);
-						focus = null;
-					}
-					//Muda o focus de figura
-					if (evt.getKeyCode() == 10){
 						
-                        for( Figure fig: figs){
-                            if ((focus == null) || (focus!=null)){
-                                focus=fig;
-								focus.cor2 = Color.green;			
-								figs.add(focus);
-								figs.remove(focus);
-								repaint();
-                                break;
-                            }
-							repaint();						
-                        }
-					}				
-					//Fim Eventos teclado
-					repaint();
-				}
-			}
-		);				
-		
-		this.addMouseListener (
-            new MouseAdapter() {
-                public void mousePressed (MouseEvent evt) {
-                    pontaMouse = getMousePosition();
-					Color foc = Color.decode("#800000");
-                    for (Figure fig: figs) {		
-						if (fig.clicked(pontaMouse.x,pontaMouse.y)) {
-							focus = fig;
-							focus.cor2 = Color.green;
-						}
-						if(focus != null) {
-							figs.add(focus);
-							figs.remove(focus);	
-						}
+						
+						break;
 					}
+		
+				}
 				} 
             }
 			);	
 			this.addMouseMotionListener(
 			new MouseMotionAdapter(){
 				public void mouseDragged (MouseEvent evt) {
-					pontaMouse = getMousePosition();
-					if (focus != null) {  
-						
-                        figs.remove(focus);
-						figs.add(focus);
-						focus.x = pontaMouse.x - focus.w;
-						focus.y = pontaMouse.y - focus.h;		
-						repaint();				
+					for (Figure fig: figs){
+						if (focus == fig) { 
+							focus.x = evt.getX() - focus.w;
+							focus.y = evt.getY() - focus.h;
+							repaint();								
+						}
 					}
 				}				
 			}
@@ -360,7 +193,16 @@ class ProjetoFrame extends JFrame{
 	public void paint (Graphics g){
 		super.paint(g);
         for (Figure fig: this.figs) {
-            fig.paint(g);
+            fig.paint(g);	
+			
+            if (fig ==  focus){
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setPaint(Color.red);
+				g2d.drawRect(fig.x-2, fig.y-2, fig.w+4,fig.h+4);							
+                g2d.setPaint(Color.black);
+             }
         }
-	}
+    }
+		
+	
 }
