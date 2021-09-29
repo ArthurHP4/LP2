86961314ea53be1dad84d84428f23ed106b380aa
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-29 04:38:20 -0300

    Update Line.java

--- ProjetoAppPart1/figures/Line.java
+++ ProjetoAppPart1/figures/Line.java
@@ -3,18 +3,18 @@ package figures;
 import java.awt.*;
 
 public class Line extends Figure {
-	int a, l;
-    public Line(int x, int y, int l,int a, Color cor1) {
-        super(x, y, cor1);
-		this.l = l;
-		this.a = a;
+    public Line(int x, int y, int w,int h, Color cor1,Color cor2) {
+		super(x,y,w,h,cor1,cor2);
+		cor2 = cor1;
+		cor1 = cor2;
     }
    
     public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(cor1);
-        g2d.drawLine(x, y, l, a);
+        g2d.drawLine(x, y, w, h);
     }
   
 }
+
 
