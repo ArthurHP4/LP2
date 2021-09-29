d741f602ba9e61926b095193d6124f49a797477a
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-09-29 04:37:54 -0300

    Update Rect.java

--- ProjetoAppPart1/figures/Rect.java
+++ ProjetoAppPart1/figures/Rect.java
@@ -3,21 +3,16 @@ package figures;
 import java.awt.*;
 
 public class Rect extends Figure{
-	int largura, altura;
-    Color cor2;
-    public Rect(int x, int y, int l, int a, Color cor1,Color cor2) {
-        super(x, y, cor1);
-		this.cor2 = cor2;
-        this.largura = l;
-        this.altura = a;
+    public Rect(int x, int y, int w, int h, Color cor1, Color cor2) {
+		super(x,y,w,h,cor1,cor2);
     }
    
     public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(cor1);
-		g2d.fillRect(x, y, largura, altura);  
+		g2d.fillRect(x, y, w, h);  
 		g2d.setColor(cor2);
-		g2d.drawRect(x, y, largura, altura);
+		g2d.drawRect(x, y, w, h);
 
     }
 }
