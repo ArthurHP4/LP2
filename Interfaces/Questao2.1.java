a8a94a3f99fddb422d8efac6806ea86b29d2023e
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-10-21 20:29:01 -0300

    Create Questao2.1.java

---
+++ Interfaces/Questao2.1.java
@@ -0,0 +1,26 @@
+Primeira interface:
+
+interface Bateria{
+    void energia(int pow);
+    void tempoRestante(int hora, int minuto);
+}
+
+Objetos representativos com a interface "Bateria":
+
+1)Celular
+2)Notebook
+3)Pilha Recarregavel
+
+
+Segunda Interface:
+
+interface Openable{
+    void open(boolean open);
+    void closed(boolean closed);
+}
+
+Objetos representativos com a interface "Open":
+
+1)Janela
+2)Armário
+3)Cofre
