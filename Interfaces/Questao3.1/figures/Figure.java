9d87330b5e28bdacd71dbd050fefe89768d2908e
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-10-24 20:19:20 -0300

    Create Figure.java

---
+++ Interfaces/Questao3.1/figures/Figure.java
@@ -0,0 +1,31 @@
+package figures;
+
+import java.awt.*;
+import ivisible.IVisible;
+
+public abstract class Figure implements IVisible{ 
+	public int x, y;
+	public int w,h;
+    public Color cor1;
+	public Color cor2;
+	public Figure (int x,int y,int w,int h,Color c1,Color c2) {
+		this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;    
+		cor1 = c1;
+		cor2 = c2;
+
+    }
+    
+    public void setColor(Color c1, Color c2) {
+    	cor1 = c1;
+		cor2 = c2;
+    }
+	
+    public abstract void paint (Graphics g);   
+	
+    public boolean clicked (int x, int y) {
+        return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
+    }
+}
