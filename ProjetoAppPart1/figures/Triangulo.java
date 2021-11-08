6b6d0bd5a586353a89f6cfa4ac726529b4d22dc4
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-11-08 20:17:31 -0300

    Update Triangulo.java

--- ProjetoAppPart1/figures/Triangulo.java
+++ ProjetoAppPart1/figures/Triangulo.java
@@ -10,15 +10,23 @@ public class Triangulo extends Figure{
 		super(x,y,w,h,cor1,cor2);
 	}
 
-	public void paint(Graphics g) {
+	public void paint(Graphics g,boolean focus) {
 		
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(cor1);
-		int xPoints[] = {this.x, ((this.w)/2) + (this.x), (this.w) + (this.x)};
-        int yPoints[] = {this.y + h, (this.y + h) - (this.h), this.y + h};
-		g2d.fillPolygon(xPoints, yPoints, 3);
-		g2d.setColor(cor2);	
-		g2d.drawPolygon(xPoints, yPoints, 3);
+		
+		if (focus == false){			
+			g2d.setColor(cor1);
+			int xPoints[] = {this.x, ((this.w)/2) + (this.x), (this.w) + (this.x)};
+			int yPoints[] = {this.y + h, (this.y + h) - (this.h), this.y + h};
+			g2d.fillPolygon(xPoints, yPoints, 3);
+			g2d.setColor(cor2);	
+			g2d.drawPolygon(xPoints, yPoints, 3);
+		}
+		else{
+			 g2d.setPaint(Color.red);
+			 g2d.drawRect(this.x-2, this.y-2, this.w+4,this.h+4);							
+             g2d.setPaint(Color.black);
+		}
 	}
 
 }
