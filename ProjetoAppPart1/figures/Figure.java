982a7cc82d59cea48051e5704dc2a7df4b58229d
ArthurHP4 <88325021+ArthurHP4@users.noreply.github.com>
2021-11-08 20:16:45 -0300

    Update Figure.java

--- ProjetoAppPart1/figures/Figure.java
+++ ProjetoAppPart1/figures/Figure.java
@@ -15,21 +15,15 @@ public abstract class Figure implements IVisible, Serializable{
         this.h = h;    
 		cor1 = c1;
 		cor2 = c2;
-
-    }
-    
+    }  
     public void setColor(Color c1, Color c2) {
     	cor1 = c1;
 		cor2 = c2;
-    }
-	
-    public abstract void paint (Graphics g);   
-	
-    public void drag (int distanceX, int distanceY) {
+    }		
+	public void drag (int distanceX, int distanceY) {
         this.x += distanceX;
         this.y += distanceY;
-    }
-    
+    }   
     public boolean clicked (int x, int y) {
         return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
     }
